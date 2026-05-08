9b1faeb0d23d9d6ade7d0fb9e2adc52686c3316c
hojn <81694153+sanspool@users.noreply.github.com>
2026-05-08 15:52:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,5 +7,10 @@ import pandas as pd
 # 1. 웹 페이지 설정
 st.set_page_config(page_title="남동고 등산 메이트", layout="wide")
 
-st.title("🏔️⛰️🗻2026 학교 등산 행사 안내 지도")
+st.title("🏔️2026 학교 등산 행사 안내 지도")
+st.markdown("우리 동아리가 직접 발로 뛰며 만든 코스 가이드 입니다.")
+st.markdown("왼쪽 메뉴에서 코스를 선택하고 행사에 참여해 보세요.")
+st.markdown("# 큰 제목 ")
+st.markdown("## 작은 제목 ")
+st.markdown("**굵은 글씨**와 *이텔릭체* ")
 st.text("userskin1")
