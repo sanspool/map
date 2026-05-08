6fb35638f98c08159a63ea539fdeaa1fb4770e74
hojn <81694153+sanspool@users.noreply.github.com>
2026-05-08 16:08:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ st.markdown("**굵은 글씨**와 *이텔릭체* ")
 st.header("헤더입니다")
 st.subheader("서브헤더입니다")
 st.caption("캡션입니다")
-st.code('print("hello world")')
+st.code("print('hello world')")
 
 st.text("userskin1")
 
