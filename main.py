236d077ef92f6480eed95a3ae336b9fc3dc059d4
hojn <81694153+sanspool@users.noreply.github.com>
2026-08-21 15:55:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -171,8 +171,6 @@ with col2:
 
     
     
-    st.info("길이 미끄럽습니다. 주의하세요.") #코스별 정보 넣기
-    st.metric(label="소요시간",value="10분") #소요시간, 정보 코스별로 넣기
-    st.write("주의사항 : 등산화를 착용하세요.")
-
-#st_folium(m, width=700, height=500)
+#    st.info("길이 미끄럽습니다. 주의하세요.") #코스별 정보 넣기
+#    st.metric(label="소요시간",value="10분") #소요시간, 정보 코스별로 넣기
+#    st.write("주의사항 : 등산화를 착용하세요.")
