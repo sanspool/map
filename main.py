cd63ffa5432c1b8af01f108f7197fb2e7fae1d53
hojn <81694153+sanspool@users.noreply.github.com>
2026-08-21 14:30:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,19 +10,13 @@ st.set_page_config(page_title="남동고 등산 메이트", layout="wide")
 st.title("🏔️2026 학교 등산 행사 안내 지도")
 st.markdown("우리 동아리가 직접 발로 뛰며 만든 코스 가이드 입니다.")
 st.markdown("왼쪽 메뉴에서 코스를 선택하고 행사에 참여해 보세요.")
-st.markdown("# 큰 제목 ")
-st.markdown("## 작은 제목 ")
-st.markdown("**굵은 글씨**와 *이텔릭체* ")
-
-st.header("헤더입니다")
-st.subheader("서브헤더입니다")
-st.caption("캡션입니다")
-st.code("print('hello world')")
-
-st.text("userskin1")
 
 # 2. 데이터 읽어오기(데이터 수집 csv)
-df = pd.read_csv('듬배산등산경로.csv', encoding='UTF-8')
+df = pd.read_csv('등산 경로.csv', encoding='UTF-8')
+# 코스의 위치에 해당하는 사진 이미지 이름: "images/A입구.jpg"
+df['이미지'] = 'images/' + df['코스'] + df['위치명'] + '.jpg'
+
+
 df_latlon = df[['위도','경도']]
 df_latlon = df_latlon.rename(columns={'위도':'lat', '경도':'lon'})
 print(df_latlon)
